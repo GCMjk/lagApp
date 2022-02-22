f8d934a95c99bf16f53f241f98129034d3b1ab34
Martin Lagalm <majok5040@gmail.com>
2022-02-22 15:51:40 -0600

    user persistence, navbar

--- src/app/@lgm-erp/core/components/navbar/navbar.component.ts
+++ src/app/@lgm-erp/core/components/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FirestoreService } from '@lgm-erp-core/services/firestore.service';
+import { AuthService } from '@lgm-erp-core/services/auth.service';
+import { IRegisterUser } from '@lgm-erp-core/interfaces/auth.interface';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  logged:boolean = false;
+  uid = null! as string;
+  userInfo: IRegisterUser = null!;
 
-  ngOnInit(): void {
+  constructor( private authService: AuthService,
+              private firestore: FirestoreService ) {  }
+
+  async ngOnInit() {
+    this.authService.stateUser().subscribe( res => {
+      if(res) {
+        this.logged = true;
+        this.getUid();
+      } else {
+        this.logged = false;
+      }
+    });
+  }
+
+  async getUid() {
+    const uid = await this.authService.getUid();
+    if(uid) {
+      this.uid = uid
+      this.getUserInfo();
+    } else {
+      console.log('No hay UID');
+    }
+  }
+
+  getUserInfo() {
+    const path = 'users';
+    const id = this.uid;
+    this.firestore.getDoc<IRegisterUser>(path, id).subscribe( res => {
+      if(res) {
+        console.log(res)
+        this.userInfo = res;
+      }
+    });
+  }
+
+  
+  logout() {
+    this.logged = false;
+    this.authService.logout();
   }
 
 }
